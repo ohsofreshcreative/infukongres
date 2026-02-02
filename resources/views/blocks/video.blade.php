61a514d4fe79f638f1415f2c93c8080d144fbf9b
ohsofreshcreative <132752926+ohsofreshcreative@users.noreply.github.com>
2026-02-02 15:01:52 +0100

    Automatyczny deploy

--- resources/views/blocks/video.blade.php
+++ resources/views/blocks/video.blade.php
@@ -9,20 +9,19 @@ $sectionId = $block->data['id'] ?? null;
 $customClass = $block->data['className'] ?? '';
 @endphp
 
-<section data-gsap-anim="section" @if($sectionId) id="{{ $sectionId }}" @endif class="video -smt {{ $block->classes }} {{ $customClass }} {{ $sectionClass }}">
+<section data-gsap-anim="section" @if($sectionId) id="{{ $sectionId }}" @endif class="b-video -smt {{ $block->classes }} {{ $customClass }} {{ $sectionClass }}">
 
 	<div class="__wrapper c-main relative">
 		<div class="__col grid grid-cols-1 lg:grid-cols-2 items-center gap-10">
-			@if (!empty($g_video['image']))
-			<div data-gsap-element="{{ $flip ? 'img-right' : 'img-left' }}" class="image-reveal-wrapper __img order1">
-				<img class="object-cover w-full __img img-xl radius-img" src="{{ $g_video['image']['url'] }}" alt="{{ $g_video['image']['alt'] ?? '' }}">
+
+			@if (!empty($g_video['video']))
+			<div class="">
+				{!! $g_video['video'] !!}
 			</div>
 			@endif
 
 			<div class="__content order2">
-				@if (!empty($g_video['image']))
 				<p data-gsap-element="subtitle" class="subtitle-s">{{ $g_video['subtitle'] }}</p>
-				@endif
 				<h2 data-gsap-element="header" class="text-white m-header">{{ $g_video['title'] }}</h2>
 
 				<div data-gsap-element="txt" class="__txt text-white">
