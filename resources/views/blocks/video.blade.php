9869694b4cf45fb7f97561761db0357c8e6a8b06
ohsofreshcreative <132752926+ohsofreshcreative@users.noreply.github.com>
2026-02-02 15:36:07 +0100

    Automatyczny deploy

--- resources/views/blocks/video.blade.php
+++ resources/views/blocks/video.blade.php
@@ -9,7 +9,7 @@ $sectionId = $block->data['id'] ?? null;
 $customClass = $block->data['className'] ?? '';
 @endphp
 
-<section data-gsap-anim="section" @if($sectionId) id="{{ $sectionId }}" @endif class="b-video -smt {{ $block->classes }} {{ $customClass }} {{ $sectionClass }}">
+<section data-gsap-anim="section" @if(!empty($section_id)) id="{{ $section_id }}" @endif class="b-video -smt {{ $block->classes }} {{ $customClass }} {{ $sectionClass }}">
 
 	<div class="__wrapper c-main relative">
 		<div class="__col grid grid-cols-1 lg:grid-cols-2 items-center gap-10">
