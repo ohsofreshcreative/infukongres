69264cac23962bba6fe6e1926552e9a1c2087229
ohsofreshcreative <132752926+ohsofreshcreative@users.noreply.github.com>
2026-02-04 09:22:19 +0100

    Automatyczny deploy

--- app/Walkers/DropdownWalker.php
+++ app/Walkers/DropdownWalker.php
@@ -11,9 +11,9 @@ class DropdownWalker extends Walker_Nav_Menu
      */
     public function start_lvl(&$output, $depth = 0, $args = null)
     {
-        $classes = 'absolute z-10 mt-2 w-max origin-top-right bg-dark b-border-light focus:outline-none';
+        $classes = 'absolute z-10 mt-2 w-max origin-top-right bg-white border-primary focus:outline-none';
         
-        $output .= "<ul x-show=\"open\" @click.away=\"open = false\" x-transition:enter=\"transition ease-out duration-200\" x-transition:enter-start=\"opacity-0 transform -translate-y-2\" x-transition:enter-end=\"opacity-100 transform translate-y-0\" x-transition:leave=\"transition ease-in duration-150\" x-transition:leave-start=\"opacity-100 transform translate-y-0\" x-transition:leave-end=\"opacity-0 transform -translate-y-2\" class=\"{$classes}\" style=\"display: none;\">";
+        $output .= "<ul x-show=\"open\" @click.away=\"open = false\" x-transition:enter=\"transition ease-out duration-200\" x-transition:enter-start=\"opacity-0 transform -translate-y-2\" x-transition:enter-end=\"opacity-100 transform translate-y-0\" x-transition:leave=\"transition ease-in duration-150\" x-transition:leave-start=\"opacity-100 transform translate-y-0\" x-transition:leave-end=\"opacity-0 transform -translate-y-2\" class=\"{$classes} rounded-xl p-2\" style=\"display: none;\">";
     }
 
     /**
@@ -29,14 +29,10 @@ class DropdownWalker extends Walker_Nav_Menu
             // Logika Alpine.js do otwierania/zamykania przy hover zostaje w <li>
             $output .= '<li x-data="{ open: false }" @mouseenter="open = true" @mouseleave="open = false" class="relative ' . esc_attr(implode(' ', $item->classes)) . '">';
             
-            // ### POCZĄTEK ZMIANY ###
-            // Zamiast <button> używamy <a> z linkiem do strony nadrzędnej.
-            // Usunęliśmy `@click`, aby kliknięcie powodowało standardową nawigację.
             $output .= '<a href="' . esc_attr($item->url) . '" class="inline-flex items-center gap-x-1 text-sm font-medium hover:text-indigo-600">';
-            $output .= esc_html($item->title);
+            $output .= apply_filters('the_title', $item->title, $item->ID);
             $output .= '<svg class="w-4 h-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true"><path fill-rule="evenodd" d="M5.23 7.21a.75.75 0 011.06.02L10 11.168l3.71-3.938a.75.75 0 111.08 1.04l-4.25 4.5a.75.75 0 01-1.08 0l-4.25-4.5a.75.75 0 01.02-1.06z" clip-rule="evenodd" /></svg>';
             $output .= '</a>';
-            // ### KONIEC ZMIANY ###
 
         }
         // Case 2: Pozostałe elementy (zwykłe linki na górze lub linki wewnątrz dropdownu)
@@ -45,13 +41,13 @@ class DropdownWalker extends Walker_Nav_Menu
 
             $link_classes = '';
             if ($depth > 0) {
-                $link_classes = 'block px-6 py-4 text-sm text-gray-700 hover:bg-blue-600';
+                $link_classes = 'block rounded-xl px-6 py-4 text-sm text-gray-700 hover:bg-primary';
             } else {
                 $link_classes = 'text-sm font-medium hover:text-indigo-600';
             }
 
             $output .= '<a href="' . esc_attr($item->url) . '" class="' . esc_attr($link_classes) . '">';
-            $output .= esc_html($item->title);
+            $output .= apply_filters('the_title', $item->title, $item->ID);
             $output .= '</a>';
         }
     }
--- app/Walkers/MobileDropdownWalker.php
+++ app/Walkers/MobileDropdownWalker.php
@@ -32,14 +32,14 @@ class MobileDropdownWalker extends Walker_Nav_Menu
             
             // Link do strony nadrzędnej – zajmuje większość miejsca.
             $output .= '<a href="' . esc_attr($item->url) . '" class="flex-grow py-1 pr-4">';
-            $output .= esc_html($item->title);
+            $output .= apply_filters('the_title', $item->title, $item->ID);
             $output .= '</a>';
             
             // Osobny przycisk ze strzałką, który rozwija/zwija submenu.
             $output .= '<button @click.stop.prevent="open = !open" class="p-2 -mr-2 shrink-0">';
             $output .= '<span class="sr-only">Rozwiń podmenu</span>';
             // Ikona strzałki, która obraca się w zależności od stanu `open`.
-            $output .= '<svg class="w-5 h-5 text-gray-500 transition-transform duration-200" :class="{ \'rotate-180\': open }" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor"><path fill-rule="evenodd" d="M5.23 7.21a.75.75 0 011.06.02L10 11.168l3.71-3.938a.75.75 0 111.08 1.04l-4.25 4.5a.75.75 0 01-1.08 0l-4.25-4.5a.75.75 0 01.02-1.06z" clip-rule="evenodd" /></svg>';
+            $output .= '<svg class="w-5 h-5 text-primary-light transition-transform duration-200" :class="{ \'rotate-180\': open }" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor"><path fill-rule="evenodd" d="M5.23 7.21a.75.75 0 011.06.02L10 11.168l3.71-3.938a.75.75 0 111.08 1.04l-4.25 4.5a.75.75 0 01-1.08 0l-4.25-4.5a.75.75 0 01.02-1.06z" clip-rule="evenodd" /></svg>';
             $output .= '</button>';
             
             $output .= '</div>';
@@ -48,7 +48,7 @@ class MobileDropdownWalker extends Walker_Nav_Menu
         else {
             $output .= '<li>';
             $output .= '<a href="' . esc_attr($item->url) . '" class="block py-1">';
-            $output .= esc_html($item->title);
+            $output .= apply_filters('the_title', $item->title, $item->ID);
             $output .= '</a>';
         }
     }
